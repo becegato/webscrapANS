339a011b3c8311bfab4d11f945bcfe145f9ca407
Pedro Mendes <pedro.mendes@abramge.com.br>
2021-10-27 17:02:04 -0300

    Versão preliminar da query no tabnet completa.

--- R/libraries.R
+++ R/libraries.R
@@ -1,4 +1,4 @@
 # install.packages("pacman")
 
-pacman::p_load(rvest, httr, usethis, janitor, tidyverse, RSQLite, DBI,
+pacman::p_load(rvest, httr, usethis, janitor, tidyverse, RSQLite, DBI, dbplyr,
                install = F)
