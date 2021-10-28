105467a9a1b0c5fe4466114ef9bd217f939744bd
Pedro Mendes <pedro.mendes@abramge.com.br>
2021-10-28 15:20:40 -0300

    - Finalização de beta de scrap do tabnet da ANS

---
+++ R/functions.R
@@ -0,0 +1,82 @@
+# limpeza de tabelas ------------------------------------------------------
+
+#' Essa função serve para limpar os dados antes de importar para a base de dados do SQLite. Necessita incluir função de inclusão automática de tags.
+
+clear <- function(x){
+  x |>
+    rvest::html_text() |>
+    stringi::stri_trans_general(id = "Latin-ASCII") |> # Remover acentos na exportação
+    tibble::as_tibble() |>
+    tidyr::separate_rows(value, sep = "\n") |> # Padrão para separar as linhas
+    dplyr::rename(item = value) |>
+    dplyr::mutate(tag = "") |> # Criando coluna para inclusão de tags
+    dplyr::slice(-n()) # Remover última linha por conta do último \n nas variáveis
+}
+
+# requisição --------------------------------------------------------------
+
+#' Essa função serve para fazer as requisições para o tabnet
+#'
+busca <- function(coluna, conteudo, linha, tipo_contratacao, uf, ano, mes){
+
+  database <- DBI::dbConnect(RSQLite::SQLite(), "base/ans-tags.db") # Conexão com a base de dados
+
+  # As variáveis de "a" a "e" servem como auxiliares para puxar os valores selecionados para consultas.
+
+  a <- database |>
+    dplyr::tbl("coluna") |>
+    dplyr::filter(item == coluna) |>
+    dplyr::pull(tag)
+
+  b <- database |>
+    dplyr::tbl("conteudo") |>
+    dplyr::filter(item == conteudo) |>
+    dplyr::pull(tag)
+
+  c <- database |>
+    dplyr::tbl("linha") |>
+    dplyr::filter(item == linha) |>
+    dplyr::pull(tag)
+
+  d <- database |>
+    dplyr::tbl("tipo_contratacao") |>
+    dplyr::filter(item == tipo_contratacao) |>
+    dplyr::pull(tag)
+
+  e <- database |>
+    dplyr::tbl("uf") |>
+    dplyr::filter(item == uf) |>
+    dplyr::pull(tag)
+
+  # URL do tabnet
+
+  tabnet_ans <- "http://www.ans.gov.br/anstabnet/cgi-bin/tabnet?dados/tabnet_br.def"
+
+  # Escolha do ano de consulta.
+
+  periodo <- glue::glue("tb_br_{ano}{mes}.dbf")
+
+  requisicao <- glue::glue("Linha={c}&Coluna={a}&Incremento={b}&Arquivos={periodo}&SSexo=TODAS_AS_CATEGORIAS__&SFaixa_et%E1ria=TODAS_AS_CATEGORIAS__&SFaixa_et%E1ria-Reajuste=TODAS_AS_CATEGORIAS__&STipo_de_contrata%E7%E3o={d}&S%C9poca_de_contrata%E7%E3o=TODAS_AS_CATEGORIAS__&SSegmenta%E7%E3o=TODAS_AS_CATEGORIAS__&SSegmenta%E7%E3o_grupo=TODAS_AS_CATEGORIAS__&SAbrg._Geogr%E1fica=TODAS_AS_CATEGORIAS__&SModalidade=TODAS_AS_CATEGORIAS__&SUF={e}&SGrande_Regi%E3o=TODAS_AS_CATEGORIAS__&SCapital=TODAS_AS_CATEGORIAS__&SInterior=TODAS_AS_CATEGORIAS__&SReg._Metropolitana=TODAS_AS_CATEGORIAS__&formato=table&mostre=Mostra")
+
+  site <- httr::POST(url = tabnet_ans,
+                     body = requisicao,
+                     timeout(20))
+
+  dados <- httr::content(site, encoding = "Latin1", as = "parsed") |> # extrair os dados da requisição
+    rvest::html_element("table") |> # elemento desejado: dados retornados da requisição
+    rvest::html_text2() |> # extração do texto da página gerada pela requisição
+    tibble::as_tibble() |>
+    tidyr::separate_rows(value, sep = "\n")
+
+  n <- 1 + dados |>
+    dplyr::slice(1) |>
+    dplyr::pull() |>
+    stringr::str_count(pattern = "\t")
+
+  dados <- dados |>
+    tidyr::separate(col = value, sep = "\t", into = paste0("x", 1:n)) |>
+    janitor::row_to_names(row_number = 1) |>
+    dplyr::slice(-1)
+
+  return(dados)
+}
---
+++ R/script.R
@@ -0,0 +1,60 @@
+# bibliotecas e funções ---------------------------------------------------
+
+source("R/libraries.R")
+source("R/functions.R")
+
+# construindo base de dados -----------------------------------------------
+
+#' Tags encontradas no site do tabnet da ANS:
+#' <http://www.ans.gov.br/anstabnet/cgi-bin/dh?dados/tabnet_br.def>
+
+linha <- html |>
+  rvest::html_element("#L") |>
+  clear() |>
+  dplyr::mutate(tag = c("Compet%EAncia", "Sexo", "Faixa_et%E1ria", "Faixa_et%E1ria-Reajuste", "Tipo_de_contrata%E7%E3o", "%C9poca_de_contrata%E7%E3o", "Segmenta%E7%E3o", "Segmenta%E7%E3o_grupo", "Abrg._Geogr%E1fica", "Modalidade", "UF", "Grande_Regi%E3o%2FUF", "Grande_Regi%E3o", "Capital", "Interior", "Reg._Metropolitana")) |>
+    readr::write_csv(glue::glue("base/linha.csv"))
+
+coluna <- html |>
+  rvest::html_element("#C") |>
+  clear() |>
+  dplyr::mutate(tag = c("--N%E3o-Ativa--", "Compet%EAncia", "Sexo", "Faixa_et%E1ria", "Faixa_et%E1ria-Reajuste", "Tipo_de_contrata%E7%E3o", "%C9poca_de_contrata%E7%E3o", "Segmenta%E7%E3o", "Segmenta%E7%E3o_grupo", "Abrg._Geogr%E1fica", "Modalidade", "UF", "Grande_Regi%E3o", "Capital", "Interior", "Reg._Metropolitana")) |>
+  readr::write_csv(glue::glue("base/coluna.csv"))
+
+conteudo <- html |>
+  rvest::html_element("#I") |>
+  clear() |>
+  dplyr::mutate(tag = c("Assist%EAncia_M%E9dica", "Excl._Odontol%F3gico")) |>
+  readr::write_csv(glue::glue("base/conteudo.csv"))
+
+tipo_contratacao <- html |>
+  rvest::html_element("#S4") |>
+  clear() |>
+  dplyr::mutate(tag = c("TODAS_AS_CATEGORIAS__", "1", "2", "3", "4", "5")) |>
+  readr::write_csv(glue::glue("base/tipo_contratacao.csv"))
+
+uf <- html |>
+  rvest::html_element("#S10") |>
+  clear() |>
+  dplyr::mutate(tag = c("TODAS_AS_CATEGORIAS__", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "29", "28")) |>
+  readr::write_csv(glue::glue("base/uf.csv"))
+
+# Importar planilhas na base SQLite "ans-tags.db"
+
+# query -------------------------------------------------------------------
+dbListTables(database) # Listando variáveis
+
+dados <- busca(coluna = "Competencia",
+               conteudo = "Assistencia Medica",
+               linha = "UF",
+               tipo_contratacao = "Individual ou Familiar",
+               uf = "Todas as categorias",
+               ano = "12",
+               mes = "06")
+
+#' Próximos passos:
+#'
+#' - Consultas múltiplas por meio de listas
+#' - Expansão de consultas
+#' - Catalogação automática de tags de tabelas da base de dados
+
+
